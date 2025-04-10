3dac2138dca9f434b2855639a1f129e10ff4a7f0
brayanfv <brayanmf1227@gmail.com>
2025-04-10 16:20:36 -0300

    finalizada

---
+++ src/main/java/spring/boot/simplesdental/dto/ContatoDTO.java
@@ -0,0 +1,23 @@
+package spring.boot.simplesdental.dto;
+
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+// LOMBOK
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ContatoDTO {
+
+    @NotBlank(message = "O nome é obrigatório")
+    private String nome;
+    private String contato;
+    private LocalDate created_date;
+    private Long profissionalId;
+}
